f993602a87207005f5486e3143bbce3d97669fa1
Tomasz Marszałek <marstomek@gmail.com>
2018-09-02 13:03:15 +0200

    expand application logic

--- proba/src/index.js
+++ proba/src/index.js
@@ -7,11 +7,16 @@ console.log(displayEl)
 const _displayChar = (char, type) => {
     const digitEl = document.createElement('div')
     digitEl.classList.add('digit')
-    if(type !== 'digit'){
+    if (type !== 'digit') {
         digitEl.classList.add(type)
     }
     digitEl.textContent = char
     displayEl.appendChild(digitEl)
+}
+
+const newLine = () => {
+    const hr = document.createElement('hr')
+    displayEl.appendChild(hr)
 }
 
 const addDigit = (digit) => {
@@ -25,7 +30,7 @@ const addSign = (s) => {
 // r-result ok-bool
 const addResult = (r, ok) => {
     // _displayChar('=', 'digit')
-    ok ? _displayChar(r, 'ok'): _displayChar(r, 'nok')
+    ok ? _displayChar(r, 'ok') : _displayChar(r, 'nok')
 }
 
 ////task maker
@@ -33,38 +38,57 @@ const addResult = (r, ok) => {
 // expr '2+3+23'
 // returns list of '2' '+' '3' '23'
 // and eval result
-const parseExpression = (expr) => {
-    let result = eval(expr)
-    let array = expr.split(' ')
-    return {
-        result: result,
-        array: array
+
+class CalclulX {
+    constructor(expr) {
+        this.result = this._parseExpression(expr)
+    }
+
+    // draw formula on te page
+    writeFormulaOnPage() {
+        this.result.array.forEach((el) => {
+            if (['+', '-'].includes(el)) {
+                addSign(el)
+            } else {
+                addDigit(el)
+            }
+        })
+        _displayChar('=', 'digit')
+    }
+
+    displayGoodResult(){
+        addResult(this.result.result, true)
+    }
+
+    displayUserResult(uresult){
+        if(uresult === this.result.result){
+            addResult(uresult, true)
+        }else{
+            addResult(uresult, false)
+            addDigit('=>')
+            addResult(this.result.result, true)
+
+        }
+    }
+
+    _parseExpression(expr) {
+        let array = expr.split(' ')
+        let result = eval(expr)
+        return {
+            result: result,
+            array: array
+        }
     }
 }
 
-// draw formula on te page
-const writeFormulaOnPage = (expr) =>{
-    const result = parseExpression(expr)
 
-    result.array.forEach((el)=> {
-        if(['+', '-'].includes(el)){
-            addSign(el)
-        }else{
-            addDigit(el)
-        }
-        console.log(el)
-    })
+// good result
+const calcul = new CalclulX('2 + 32 + 7')
+calcul.writeFormulaOnPage()
+calcul.displayUserResult(41)
 
-    _displayChar('=', 'digit')
+newLine()
 
-    // addResult(result.result)
-    // addResult('_', true)
-
-}
-
-writeFormulaOnPage('2 + 2 - 7')
-
-// addDigit(3)
-// addSign('+')
-// addDigit(3)
-// addResult(' 1', true)
+const calcul2 = new CalclulX('2 + 32 + 7')
+calcul2.writeFormulaOnPage()
+calcul2.displayUserResult(21)
