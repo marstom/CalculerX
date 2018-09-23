94117888dc8a48e7f4c4ef0ea2be282776edaa05
Tomasz Marszałek <marstomek@gmail.com>
2018-09-23 20:37:55 +0200

    test vue view application with router.

---
+++ test2/src/views/Me.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Hi this is my view :D {{ t }}</h1>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        data(){
+            return {
+                t:'heelllooo'
+            }
+        }
+    }
+</script>
